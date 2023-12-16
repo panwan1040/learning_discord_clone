4d26c70177a44fc7599fc1197c5078363209ca23
Bas [Panwan] <panwan.21st@gmail.com>
2023-12-16 12:47:44 +0700

    no message

--- discord-clone/app/page.tsx
+++ discord-clone/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div >
       <p className='text-3xl font-bold text-indigo-500'>
-        Hello Discrod Clonehello
+        Hello Discrod Clone hello
       </p>
       <Button>
         Click me
