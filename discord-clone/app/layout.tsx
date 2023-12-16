641cf9977109174277910037892ddcff48c4393e
Bas [Panwan] <panwan.21st@gmail.com>
2023-12-16 15:14:15 +0700

    add dark mode

--- discord-clone/app/layout.tsx
+++ discord-clone/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from '@/components/provider/theme-provider'
+import { cn } from '@/lib/utils'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -15,8 +18,22 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressContentEditableWarning>
+        <body className={cn(
+          font.className,
+          "bg-white dark:bg-[#333333]"
+        ) }>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='dark'
+            enableSystem={false}
+            storageKey='discord-theme'
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
