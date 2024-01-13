5e87dfb4261d811df62f599bc0819d86db2b9b38
Bas [Panwan] <panwan.21st@gmail.com>
2024-01-13 17:12:45 +0700

    Refactoring member to members

--- discord-clone/app/api/servers/route.ts
+++ discord-clone/app/api/servers/route.ts
@@ -26,7 +26,7 @@ export async function POST(req: Request) {
                         { name: "general", profileId: profile.id }
                     ]
                 },
-                member: { 
+                members: { 
                     create: [
                         { profileId: profile.id, role: MemberRole.ADMIN }
                     ]
