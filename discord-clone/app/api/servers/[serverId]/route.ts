514f6cabe5d9b698c593dc8599929c8347d2f118
Bas [Panwan] <panwan.21st@gmail.com>
2024-01-23 16:29:42 +0700

    delete server

--- discord-clone/app/api/servers/[serverId]/route.ts
+++ discord-clone/app/api/servers/[serverId]/route.ts
@@ -2,6 +2,34 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+
+export async function DELETE(
+    req: Request,
+    { params }: { params: { serverId: string }}
+) {
+    try {
+        const profile = await currentProfile();
+
+        if (!profile) {
+            return new NextResponse("Unathorized", { status: 401});
+        }
+
+        const server = await db.server.delete({
+            where: {
+                id: params.serverId,
+                profileId: profile.id
+            },
+
+        });
+
+        return NextResponse.json(server);
+    } catch (error) {
+        console.log("[SERVER_ID_DELETE]", error);
+        return new NextResponse("Internal Error", { status: 500 })
+    }
+}
+
+
 export async function PATCH(
     req: Request,
     { params }: { params: { serverId: string }}
@@ -27,7 +55,7 @@ export async function PATCH(
 
         return NextResponse.json(server);
     } catch (error) {
-        console.log("[SERVER_IDPATCH]", error);
+        console.log("[SERVER_ID_PATCH]", error);
         return new NextResponse("Internal Error", { status: 500 })
     }
 }
