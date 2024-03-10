77d6d4ffbd837595219b3d99c17a2da4d2916432
Bas [Panwan] <panwan.21st@gmail.com>
2024-03-10 16:49:27 +0700

    update: chat -add emoji

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -14,9 +14,11 @@ import {
 } from "@/components/ui/form";
 
 import { Input } from "@/components/ui/input";
-import { Plus, Smile } from "lucide-react";
+import { Plus } from "lucide-react";
 import queryString from "query-string";
 import { useModal } from "@/hooks/use-modal-store";
+import { EmojiPicker } from "@/components/emoji-picker";
+import { useRouter } from "next/navigation";
 
 interface ChatInputProps {
     apiUrl: string;
@@ -36,6 +38,7 @@ export const ChatInput = ({
     type
 }: ChatInputProps) => {
     const { onOpen } = useModal();
+    const router = useRouter();
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -54,12 +57,16 @@ export const ChatInput = ({
             });
 
             await axios.post(url, values);
+
+            form.reset();
+            router.refresh();
         } catch (error) {
             console.log(error);
             
         }
         
     }
+    
     return (
         <Form {...form} >
             <form onSubmit={form.handleSubmit(onSubmit)}>
@@ -87,7 +94,9 @@ export const ChatInput = ({
                                         {...field}
                                     />
                                     <div className="absolute top-7 right-8">
-                                        <Smile />
+                                        <EmojiPicker 
+                                            onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
+                                        />
                                     </div>
                                 </div>
                             </FormControl>
