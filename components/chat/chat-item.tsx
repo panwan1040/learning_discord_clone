c2e31b068d2acbe5b2ca18a8fdf34e9a4211fbd5
Bas [Panwan] <panwan.21st@gmail.com>
2024-03-16 17:13:25 +0700

    update: chat -realtime chat / edit

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -5,6 +5,8 @@ import axios from "axios";
 import qs from "query-string";
 import { zodResolver } from "@hookform/resolvers/zod";
 
+import { useRouter, useParams }  from "next/navigation";
+
 
 import { Member, MemberRole, Profile } from "@prisma/client";
 
@@ -68,6 +70,16 @@ export const ChatItem = ({
 }: ChatItemProps) => {
     const [isEditing, setIsEditing] = useState(false);
     const { onOpen } = useModal();
+    const params = useParams();
+    const router = useRouter();
+
+    const onMemberClick = () => {
+        if (member.id === currentMember.id) {
+            return;
+        }
+
+        router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+    }
 
     useEffect(() => {
         const handleKeyDown = (event: any) => {
@@ -127,13 +139,13 @@ export const ChatItem = ({
     return (
         <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
             <div className="group flex gap-x-2 items-start w-full">
-                <div className="cursor-pointer hover:drop-shadow-md transition">
+                <div onClick={onMemberClick} className="cursor-pointer hover:drop-shadow-md transition">
                     <UserAvatar src={member.profile.imageUrl} />
                 </div>
                 <div className="flex flex-col w-full">
                     <div className="flex items-center gap-x-2">
                         <div className="flex items-center">
-                            <p className="font-semibold text-sm hover:underline">
+                            <p onClick={onMemberClick} className="font-semibold text-sm hover:underline cursor-pointer">
                                 {member.profile.name}
                             </p>
                             <ActionTooltip label={member.role}>
@@ -149,8 +161,7 @@ export const ChatItem = ({
                          href={fileUrl}
                          target="_blank"
                          rel="noopener noreferrer"
-                         className="relative aspect-square rounded-md mt-2 overflow-hidden border flex
-                         items-center bg-secondary h-48 w-48"
+                         className="relative aspect-square rounded-md mt-2 overflow-hidden border flex items-center bg-secondary h-48 w-48"
                         >
                             <Image 
                                 src={fileUrl}
@@ -203,8 +214,8 @@ export const ChatItem = ({
                                                 <div className="relative w-full">
                                                     <Input 
                                                         disabled={isLoading}
-                                                        className="p-2 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0
-                                                        focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
+                                                        className="p-2 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 
+                                                        focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
                                                         placeholder="Edited message"
                                                         {...field}
                                                     />
@@ -225,14 +236,12 @@ export const ChatItem = ({
                 </div>
             </div>
             {canDeleteMessage && (
-                <div className="hidden group-hover:flex items-center gap-x-2 absolute
-                p-1 -top-2 right-5 bg-white dark:bg-zinc-800 border rounded-sm">
+                <div className="hidden group-hover:flex items-center gap-x-2 absolute p-1 -top-2 right-5 bg-white dark:bg-zinc-800 border rounded-sm">
                     {canEditMessage && (
                         <ActionTooltip label="Edit">
                             <Edit 
                             onClick={() => setIsEditing(true)}
-                                className="cursor-pointer ml-auto w-4 h-4 text-zinc-500
-                                hover:text-zinc-600 dark:hover:text-zinc-300 transition"
+                                className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition"
                             />
                         </ActionTooltip>
                     )}
@@ -242,8 +251,7 @@ export const ChatItem = ({
                                     apiUrl: `${socketUrl}/${id}`,
                                     query: socketQuery,
                                 })}
-                                className="cursor-pointer ml-auto w-4 h-4 text-zinc-500
-                                hover:text-zinc-600 dark:hover:text-zinc-300 transition"
+                                className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition"
                             />
                         </ActionTooltip>
                 </div>
--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -7,6 +7,7 @@ import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
 import { format } from "date-fns";
 import { ChatItem } from "./chat-item";
+import { useChatSocket } from "@/hooks/use-chat-scoket";
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
@@ -40,6 +41,8 @@ export const ChatMessages = ({
     type
 }: ChatMessagesProps) => {
     const queryKey = `chat:${chatId}`;
+    const addKey = `chat:${chatId}:messages`;
+    const updateKey = `chat:${chatId}:message:update`;
 
     const {
         data,
@@ -53,6 +56,8 @@ export const ChatMessages = ({
         paramKey,
         paramValue
     });
+
+    useChatSocket({ queryKey, addKey, updateKey})
 
     if (status === "pending") {
         return (
